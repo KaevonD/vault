086a4dae1e0d028fd4ce7849bb5305a559e85b40
Alexander Scheel <alex.scheel@hashicorp.com>
2022-06-23 15:14:24 -0400

    Use issuer's SignatureAlgorithm for CRL signing
    Presumably the issuer's SignatureAlgorithm was chosen for a reason. With
    PSS support, this could mean the underlying key is using a PKCS#11
    mechanism. Alternatively, a longer hash function was chosen, aligning
    with some other requirements (ECDSA size, &c).
    
    Rather than leaving it up to the Go library to choose signature
    algorithm, proactively mirror it from the issuer's certificate.
    
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>

--- builtin/logical/pki/crl_util.go
+++ builtin/logical/pki/crl_util.go
@@ -622,6 +622,7 @@ WRITE:
 		Number:              big.NewInt(crlNumber),
 		ThisUpdate:          time.Now(),
 		NextUpdate:          time.Now().Add(crlLifetime),
+		SignatureAlgorithm:  signingBundle.Certificate.SignatureAlgorithm,
 	}
 
 	crlBytes, err := x509.CreateRevocationList(rand.Reader, revocationListTemplate, signingBundle.Certificate, signingBundle.PrivateKey)
